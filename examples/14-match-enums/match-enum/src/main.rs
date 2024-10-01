fddbf9b61a8c0a8adc3dc7d75772606d81e7fdb0
Papadritta <papadritta@gmail.com>
2024-10-01 22:31:10 +0700

    Update main.rs
    add exercises Applied Enums for Rust

--- examples/14-match-enums/match-enum/src/main.rs
+++ examples/14-match-enums/match-enum/src/main.rs
@@ -1,16 +1,18 @@
 enum FileSize {
-    Bytes(u64),
+    Bytes(f64),
     Kilobytes(f64),
     Megabytes(f64),
     Gigabytes(f64),
+    Terabytes(f64),
 }
 
-fn format_size(size: u64) -> String {
+fn format_size(size: f64) -> String {
     let filesize = match size {
-        0..=999 => FileSize::Bytes(size),
-        1000..=999_999 => FileSize::Kilobytes(size as f64 / 1000.0),
-        1_000_000..=999_999_999 => FileSize::Megabytes(size as f64 / 1_000_000.0),
-        _ => FileSize::Gigabytes(size as f64 / 1_000_000_000.0),
+        0.0..=999.0 => FileSize::Bytes(size),
+        1000.0..=999_999.0 => FileSize::Kilobytes(size / 1000.0),
+        1_000_000.0..=999_999_999.0 => FileSize::Megabytes(size / 1_000_000.0),
+        1_000_000_000.0..=999_999_999_999.0 => FileSize::Gigabytes(size / 1_000_000_000.0),
+        _ => FileSize::Terabytes(size / 1_000_000_000_000.0),
     };
 
     match filesize {
@@ -18,11 +20,11 @@ fn format_size(size: u64) -> String {
         FileSize::Kilobytes(kb) => format!("{:.2} KB", kb),
         FileSize::Megabytes(mb) => format!("{:.2} MB", mb),
         FileSize::Gigabytes(gb) => format!("{:.2} GB", gb),
+        FileSize::Terabytes(tb) => format!("{:.2} TB", tb),
     }
 }
 
-
 fn main() {
-    let result = format_size(6888837399);
+    let result = format_size(2.44);
     println!("{}", result)
 }
