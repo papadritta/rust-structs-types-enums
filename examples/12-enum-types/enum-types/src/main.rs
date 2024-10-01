4846ab7421905ebbbc43fe0e0d4b9875fd6e608a
Papadritta <papadritta@gmail.com>
2024-10-01 21:50:23 +0700

    Update main.rs
    add enum as a type for rust exercises

--- examples/12-enum-types/enum-types/src/main.rs
+++ examples/12-enum-types/enum-types/src/main.rs
@@ -1,11 +1,12 @@
 #[derive(Debug)]
 enum WineRegions {
     Bordeaux,
-    Burgundy,
-    Champagne,
+    // Burgundy,
+    // Champagne,
     Tuscany,
     Rioja,
-    NapaValley,
+    // NapaValley,
+    Australia,
 }
 
 struct Wine {
@@ -20,6 +21,13 @@ fn supported_regions(w: WineRegions) {
     }
 }
 
+fn popularity(p: WineRegions) {
+    match p {
+        WineRegions::Bordeaux => println!("Bordeaux - Highly Popular!"),
+        _ => println!("{:?} is not popular!", p),
+    }
+}
+
 fn main() {
     let wine1 = Wine {
         name: String::from("Chateau Margaux"),
@@ -31,8 +39,19 @@ fn main() {
         region: WineRegions::Tuscany,
     };
 
-    // println!("Wine 1: {} from {:?}", wine1.name, wine1.region);
-    // println!("Wine 2: {} from {:?}", wine2.name, wine2.region);
+    let wine3: Wine = Wine {
+        name: String::from("BoraBora"),
+        region: WineRegions::Australia,
+    };
+
+    println!("Wine 1: {} from {:?}", wine1.name, wine1.region);
+    println!("Wine 2: {} from {:?}", wine2.name, wine2.region);
+    println!("Wine 3: {} from {:?}", wine3.name, wine3.region);
     supported_regions(wine1.region);
     supported_regions(WineRegions::Rioja);
+    popularity(WineRegions::Bordeaux);
+    popularity(WineRegions::Australia);
 }
+
+// Modify the code to add a new variant to the `WineRegions` enum, representing a wine region of your choice. Create a new `wine3` instance that utilizes this new variant. Print information about `wine3` to verify your changes.
+// Extend the program by implementing a function that takes a `WineRegions` value as input and returns a string representation of the region's popularity. For example, for the region "Bordeaux", the function could return "Highly Popular." Invoke this function with different wine regions and print the result to ensure it works correctly.
